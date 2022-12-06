d20d9f209a9ae8219df452eb6a0963e089032123
checkerschaf <git@janpoth.de>
2022-12-06 12:43:08 +0100

    finish day 3

---
+++ 2022/day/3/part1.ts
@@ -0,0 +1,40 @@
+const input = await Deno.readTextFile("input.txt");
+
+const rucksacks = input
+  .split("\n")
+  .map((line) => [
+    line.slice(0, line.length / 2),
+    line.slice(line.length / 2, line.length),
+  ]);
+
+const common = (a: string, b: string): string => {
+  for (let i = 0; i < a.length; i++) {
+    if (b.indexOf(a[i]) !== -1) {
+      return a[i];
+    }
+  }
+  throw new Error(`No common letter found in ${a} and ${b}`);
+};
+
+const commonLetters = rucksacks.map((rucksack) =>
+  common(rucksack[0], rucksack[1])
+);
+
+const priority = (char: string): number => {
+  const ascii = char.charCodeAt(0);
+  if (ascii >= 97 && ascii <= 122) {
+    return ascii - 96;
+  }
+  if (ascii >= 65 && ascii <= 90) {
+    return ascii - 64 + 26;
+  }
+  throw new Error("Invalid character");
+};
+
+const priorities = commonLetters.map((commonLetters) =>
+  priority(commonLetters)
+);
+
+const sumOfPriorities = priorities.reduce((acc, priority) => acc + priority, 0);
+
+console.log(sumOfPriorities);
---
+++ 2022/day/3/part2.ts
@@ -0,0 +1,42 @@
+const input = await Deno.readTextFile("input.txt");
+
+const rucksacks = input.split("\n");
+
+const rucksackChunks = [];
+const chunkSize = 3;
+for (let i = 0; i < rucksacks.length; i += chunkSize) {
+  const chunk = rucksacks.slice(i, i + chunkSize);
+  rucksackChunks.push(chunk);
+}
+
+const common = (a: string, b: string, c: string): string => {
+  for (let i = 0; i < a.length; i++) {
+    if (b.indexOf(a[i]) !== -1 && c.indexOf(a[i]) !== -1) {
+      return a[i];
+    }
+  }
+  throw new Error(`No common letter found in ${a}, ${b} and ${c}`);
+};
+
+const commonLetters = rucksackChunks.map((chunk) =>
+  common(chunk[0], chunk[1], chunk[2])
+);
+
+const priority = (char: string): number => {
+  const ascii = char.charCodeAt(0);
+  if (ascii >= 97 && ascii <= 122) {
+    return ascii - 96;
+  }
+  if (ascii >= 65 && ascii <= 90) {
+    return ascii - 64 + 26;
+  }
+  throw new Error("Invalid character");
+};
+
+const priorities = commonLetters.map((commonLetters) =>
+  priority(commonLetters)
+);
+
+const sumOfPriorities = priorities.reduce((acc, priority) => acc + priority, 0);
+
+console.log(sumOfPriorities);
